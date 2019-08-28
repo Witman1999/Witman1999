5b8d40c4d8db9e651beeb952314beed20bd92785
Witman1999 <1515384539@qq.com>
2019-08-28 17:00:02 +0800

    Site updated: 2019-08-28 17:00:01

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/about/index.html","6d1fa53a1a04bcfddc5fadbe5e17e087"],["/archives/index.html","fdf3e384c2fba2d745a4a55778d53a13"]];
+var precacheConfig = [["/about/index.html","c40111ff20378537b7fa2baab9b2b3cd"],["/archives/index.html","777174c133926222ba2b6fc3dfe2d031"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
